72e6e0b911d2fe4e469641318ddda9e6eeb7e5e6
outdoteth <outdoteth@users.noreply.github.com>
2023-01-12 18:57:22 +0000

    Add royalty info for fake nft

--- script/CreateFakeNft.sol
+++ script/CreateFakeNft.sol
@@ -1,15 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.17;
 
 import "forge-std/Script.sol";
 import "forge-std/console.sol";
 import "ERC721A/ERC721A.sol";
+import "openzeppelin/token/common/ERC2981.sol";
 
-contract FakeNft is ERC721A {
+contract FakeNft is ERC721A, ERC2981 {
     string public baseUri;
+    address public immutable owner;
 
     constructor(string memory name, string memory symbol, string memory baseUri_) ERC721A(name, symbol) {
         baseUri = baseUri_;
+        owner = msg.sender;
     }
 
     function mint(address to, uint256 quantity) public {
@@ -18,6 +21,14 @@ contract FakeNft is ERC721A {
 
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
         return string(abi.encodePacked(baseUri, _toString(tokenId)));
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC2981, ERC721A) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function royaltyInfo(uint256 tokenId, uint256 salePrice) public view override returns (address, uint256) {
+        return (owner, salePrice * 250 / 10_000); // 2.5% royalty
     }
 }
 
